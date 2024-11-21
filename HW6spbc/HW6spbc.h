0066c15d16ce27a5e5afb3fd5672ff535882b68c
Joel Burlingame <jtburlingame777@gmail.com>
2024-11-20 20:49:22 -0800

    Add: updated scanFile to scanCityFile and added scanRoadFile

--- HW6spbc/HW6spbc.h
+++ HW6spbc/HW6spbc.h
@@ -4,14 +4,18 @@
 #pragma once
 
 #include "HW6spbc.h"
+#include "GraphCities.h"
 #include <iostream>
 #include <chrono>
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
-void scanFile(string filename);
+void scanCityFile(string filename, unordered_map<string, City>& cities);
+void scanRoadsFile(const string& filename, GraphCities& graph, const unordered_map<string, City>& cities);
+
 
 // TODO: Reference additional headers your program requires here.
